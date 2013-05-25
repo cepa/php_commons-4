d32d0214f374952a5b57acb031eef481669a5c1f
cepa <cepa@e588dd84-e1f6-4f18-b586-b92c208bf0ea>
2013-05-25 19:41:00 +0000

    Commons # http plugin redirect
    git-svn-id: http://source/svn/php_commons/branches/php_commons-4-github@2142 e588dd84-e1f6-4f18-b586-b92c208bf0ea

---
+++ src/Commons/Http/Plugin/RedirectPlugin.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * =============================================================================
+ * @file        Commons/Http/Plugin/RedirectPlugin.php
+ * @author     Lukasz Cepowski <lukasz@cepowski.com>
+ *
+ * @copyright  PHP Commons
+ *              Copyright (C) 2009-2012 HellWorx Software
+ *              All rights reserved.
+ *              www.hellworx.com
+ * =============================================================================
+ */
+
+namespace Commons\Http\Plugin;
+
+use Commons\Http\StatusCode;
+use Commons\Plugin\AbstractPlugin;
+
+class RedirectPlugin extends AbstractPlugin
+{
+    
+    protected $_isExitEnabled = true;
+    
+    public function enableExit($bool = true)
+    {
+        $this->_isExitEnabled = $bool;
+        return $this;
+    }
+    
+    public function redirect($asset)
+    {
+        $invoker = $this->getInvoker();
+        if (preg_match('#^[a-zA-Z]+\:\/\/#', $asset)) {
+            $url = $asset;
+        } else {
+            $url = $invoker->assetUrl($asset);
+        }
+        $invoker->getResponse()
+            ->setStatus(StatusCode::HTTP_FOUND)
+            ->setHeader('Location', $url);
+        if ($this->_isExitEnabled) {
+            $invoker->getResponse()
+                ->clearBody()
+                ->send();
+            exit;
+        }
+        return $invoker;
+    }
+    
+}
+
