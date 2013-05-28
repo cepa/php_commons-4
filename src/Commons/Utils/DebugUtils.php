d06aa43185c2f7552d3f0e0ed0232f668469ed0f
cepa <cepa@e588dd84-e1f6-4f18-b586-b92c208bf0ea>
2013-05-28 17:13:55 +0000

    Commons # missing DebugUtils
    git-svn-id: http://source/svn/php_commons/branches/php_commons-4-github@2179 e588dd84-e1f6-4f18-b586-b92c208bf0ea

---
+++ src/Commons/Utils/DebugUtils.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * =============================================================================
+ * @file        Commons/Utils/DebugUtils.php
+ * @author     Lukasz Cepowski <lukasz@cepowski.com>
+ * 
+ * @copyright  PHP Commons
+ *              Copyright (C) 2009-2012 HellWorx Software
+ *              All rights reserved.
+ *              www.hellworx.com
+ * =============================================================================
+ */
+
+namespace Commons\Utils;
+
+use Commons\Log\Log;
+
+class DebugUtils
+{
+
+    protected function __construct() {}
+
+    public static function renderExceptionPage(\Exception $e)
+    {
+        ob_end_clean();
+        
+        $message = get_class($e).": ".$e->getMessage()."\n".$e->getTraceAsString();
+        Log::critical(str_replace("\n", ': ', $message));
+        
+        header("Content-Type: text/html; charset=utf-8");
+        ?>
+        <html>
+        <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Fatal Error</title>
+        </head>
+        <body style="border:0; margin:0; padding:0; width:100%; height:100%; font:13px Arial,Tahoma,helvetica,sans-serif; background:#efefef; color:#000000;">
+        <div style="height:70px; background:#b70606; color:#ffffff; line-height:70px; font-size:30px; font-weight:bold; padding:0 20px;">Error</div>
+        <div style="padding:10px 20px;">
+            <div style="font-weight:bold; font-size:16px; color:#aaaaaa; line-height:32px; letter-spacing:1px;">Error:</div>
+            <div style="font-size:22px; font-weight:bold; color:#000000; padding:4px 20px;"><?= get_class($e) ?>: <?= $e->getMessage() ?></div>
+            <div style="font-weight:bold; font-size:16px; color:#aaaaaa; line-height:32px; letter-spacing:1px;">Backtrace:</div>
+            <div style="padding:4px 20px;">
+                <pre><?= $e->getTraceAsString() ?></pre>
+            </div>
+        </div>
+        <div style="text-align:center; padding:20px; font-size:10px; color:#444444;">&copy; <?= date('Y') ?> <a href="http://www.hellworx.com">HellWorx Software</a></div>
+        </body>
+        </html>
+        <?   
+    }
+
+}
