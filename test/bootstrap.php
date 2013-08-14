7737c277a41b5d9bee58bf9bd190ced718a9c462
cepa <cepa@e588dd84-e1f6-4f18-b586-b92c208bf0ea>
2013-08-14 16:35:16 +0000

    Commons # deprecate old sql utils to support PHP-5.5.x
    git-svn-id: http://source/svn/php_commons/branches/php_commons-4-github@2466 e588dd84-e1f6-4f18-b586-b92c208bf0ea

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -76,53 +76,25 @@ class Bootstrap
             'password'  => DATABASE_PASSWORD
         );
     }
+    
+    public static function executeSqlQuery($query, $database = null)
+    {
+        $conn = new \Commons\Sql\Connection\Connection(new \Commons\Sql\Driver\PdoDriver());
+        $conn->connect(self::getDatabaseOptions($database));
+        $conn->prepareStatement($query)->execute();
+        $conn->disconnect();
+    }
 
     public static function createDatabase()
     {
-        $database = 'test_'.\Commons\Utils\RandomUtils::randomString(10);
-        switch (DATABASE_DRIVER) {
-            case 'mysql':
-                \Commons\Sql\Utils\MysqlUtils::createDatabase(
-                    $database,
-                    DATABASE_HOST,
-                    DATABASE_PORT,
-                    DATABASE_USERNAME,
-                    DATABASE_PASSWORD);
-                break;
-            case 'pgsql':
-                \Commons\Sql\Utils\PgsqlUtils::createDatabase(
-                    $database,
-                    DATABASE_HOST,
-                    DATABASE_PORT,
-                    DATABASE_USERNAME,
-                    DATABASE_PASSWORD);
-                break;
-            default: throw new \RuntimeException();
-        }
+        $database = 'test_'.\Commons\Utils\RandomUtils::randomString(32);
+        self::executeSqlQuery("CREATE DATABASE {$database}");
         return $database;
     }
 
     public static function dropDatabase($database)
     {
-        switch (DATABASE_DRIVER) {
-            case 'mysql':
-                \Commons\Sql\Utils\MysqlUtils::dropDatabase(
-                    $database,
-                    DATABASE_HOST,
-                    DATABASE_PORT,
-                    DATABASE_USERNAME,
-                    DATABASE_PASSWORD);
-                break;
-            case 'pgsql':
-                \Commons\Sql\Utils\PgsqlUtils::dropDatabase(
-                    $database,
-                    DATABASE_HOST,
-                    DATABASE_PORT,
-                    DATABASE_USERNAME,
-                    DATABASE_PASSWORD);
-                break;
-            default: throw new \RuntimeException();
-        }
+        self::executeSqlQuery("DROP DATABASE IF EXISTS {$database}");
     }
 
     public static function abort(\Exception $e = null)
